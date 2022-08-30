39ee49fadb0d62e3ae59f96893ce607011bd4390
aladnansami <aladnansami21@gmail.com>
2022-08-30 17:04:19 +0600

    yes

--- practice.js
+++ practice.js
@@ -10,3 +10,23 @@ console.log(typeof(number));
 let number1 ="1"
 console.log(typeof(number1))
 
+// appand
+
+let firstName ="adnan"
+let lastName = "sami"
+console.log(firstName+" "+lastName)
+
+// parsh and color change
+
+let a ="10.2"
+let b = parseFloat(10.2)
+console.log(a)
+console.log(b)
+
+// true false
+
+let a1 = 2
+let a2 = 3
+console.log(a==b)
+
+//
