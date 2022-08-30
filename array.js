39690e13cd724f6d12bfec14fcdadf0409a6f0d5
aladnansami <aladnansami21@gmail.com>
2022-08-30 18:04:35 +0600

    2nd commit

---
+++ array.js
@@ -0,0 +1,6 @@
+var arr = [1,2,3,4]
+var sum = 0
+for(let i=0; i<arr.length;i++){
+    sum+=arr[i];
+}
+console.log(sum)
---
+++ mystring.js
@@ -0,0 +1,6 @@
+var str="ami tumake valobashi"
+var count = str.split(" ").length
+var words = str.slice(" ")
+
+console.log(count)
+console.log(words)
